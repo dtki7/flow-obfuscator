a6ac0a62f110ea3a76ce91d48f0fd2bed88002af
oli <oli@server>
2021-01-29 12:10:06 +0100

    disassemblable (almost there)

--- FlowObfuscator.cpp
+++ FlowObfuscator.cpp
@@ -52,7 +52,7 @@ std::vector<Instruction*> getAllInstructions(BasicBlock *basicBlock) {
 	return bbInstrs;
 }
 
-std::vector<Function*> getAllFunctions(Module& M, bool onlyFromMain = true) {
+std::vector<Function*> getAllFunctions(Module& M, bool onlyFromMain = false) {
 	std::vector<Function*> functions;
 	if (onlyFromMain) {
 		std::vector<Function*> tmpFuncs({M.getFunction("main")});
@@ -79,17 +79,18 @@ std::vector<Function*> getAllFunctions(Module& M, bool onlyFromMain = true) {
 PreservedAnalyses FlowObfuscatorPass::run(Module &M, ModuleAnalysisManager &AM) {
 	auto &ctx = M.getContext();
 
-	auto functions = getAllFunctions(M);
+	auto functions = getAllFunctions(M, true);  // TODO: not only from main
 
 	int i = 0;  // DEBUG
 	for (auto function : functions) {
 		if (function->isDeclaration()) continue;
 
-		// if (function->getName().compare("main")) continue;  // DEBUG
+		if (function->getName().compare("main")) continue;  // DEBUG
 		// if (++i > 159) continue;  // DEBUG
 
 		outs() << "function: " << function->getName() << "\n";
 
+		IRBuilder<> generalBuilder(ctx);
 		auto basicBlocks = getAllBasicBlocks(function);
 
 		// handle phi nodes
@@ -107,9 +108,9 @@ PreservedAnalyses FlowObfuscatorPass::run(Module &M, ModuleAnalysisManager &AM) 
 					auto val = phi->getIncomingValue(i);
 					if (isa<Instruction>(val)) {
 						auto incomingInstr = cast<Instruction>(val);
-						IRBuilder<> builder(incomingInstr);
-						createLoader(phi, globVar, builder);
-						createStore(incomingInstr, globVar, builder);
+						generalBuilder.SetInsertPoint(incomingInstr);
+						createLoader(phi, globVar, generalBuilder);
+						createStore(incomingInstr, globVar, generalBuilder);
 					} else if (isa<Constant>(val)) {
 						globVar->setInitializer(cast<Constant>(val));
 					} else {
@@ -130,6 +131,7 @@ PreservedAnalyses FlowObfuscatorPass::run(Module &M, ModuleAnalysisManager &AM) 
 			builder.SetInsertPoint(argBlock);
 			builder.CreateStore(&arg, globVar);
 		}
+
 		auto mainBlock = BasicBlock::Create(ctx, "main", function);
 		IRBuilder<> mainBuilder(mainBlock);
 		builder.CreateBr(mainBlock);
@@ -137,6 +139,8 @@ PreservedAnalyses FlowObfuscatorPass::run(Module &M, ModuleAnalysisManager &AM) 
 		//
 		// make variables global
 		//
+		GlobalVariable *retMutex = nullptr;
+		GlobalVariable *retVal = nullptr;
 		for (auto &basicBlock : basicBlocks) {
 			IRBuilder<> builder(basicBlock);
 
@@ -180,45 +184,97 @@ PreservedAnalyses FlowObfuscatorPass::run(Module &M, ModuleAnalysisManager &AM) 
 
 			// delete branch and append ret if necessary
 			auto lastInstr = &*--basicBlock->end();
-			if (isa<BranchInst>(lastInstr)) {
-				lastInstr->eraseFromParent();
-				lastInstr = &*--basicBlock->end();
-			}
+			// if (isa<BranchInst>(lastInstr)) {
+			// 	// TODO: create branch mutexes
+			// 	lastInstr->eraseFromParent();
+			// 	lastInstr = &*--basicBlock->end();
+			// }
 			if (!lastInstr->isTerminator() || isa<InvokeInst>(lastInstr)) {
 				builder.SetInsertPoint(basicBlock);
 				builder.CreateRetVoid();
 			}
+
+			if (isa<ReturnInst>(lastInstr)) {
+				auto retInstr = cast<ReturnInst>(lastInstr);
+				if (!retMutex) {
+					retMutex = createGlobal(M, M.getTypeByName("union.pthread_mutex_t"));
+					retVal = createGlobal(M, retInstr->getReturnValue()->getType());
+				}
+
+				// substitute return instr with store instr
+				builder.SetInsertPoint(lastInstr);
+				builder.CreateStore(retInstr->getReturnValue(), retVal);
+
+				// create mutex and call to pthread_mutex_unlock
+				builder.CreateCall(M.getFunction("pthread_mutex_unlock"), ArrayRef<Value*>(retMutex));
+
+				builder.CreateRetVoid();
+				lastInstr->eraseFromParent();
+			}
 		}
+
+		// create call to pthread_mutex_init and pthread_mutex_lock
+		if (retMutex) {
+			std::vector<Value*> args;
+			args.push_back(retMutex);
+			args.push_back(ConstantPointerNull::get(PointerType::get(M.getTypeByName("union.pthread_mutexattr_t"), 0)));
+			mainBuilder.CreateCall(M.getFunction("pthread_mutex_init"), args);
+			mainBuilder.CreateCall(M.getFunction("pthread_mutex_lock"), ArrayRef<Value*>(retMutex));
+		}
+
+		// handle synchronization
+		std::map<BasicBlock*, std::vector<BasicBlock*>> block2sync;
+		for (auto basicBlock : basicBlocks) {
+			if (basicBlock->hasNPredecessorsOrMore(1)) {
+				auto mutex = createGlobal(M, M.getTypeByName("union.pthread_mutex_t"));
+
+				auto firstInstr = basicBlock->getFirstNonPHI();
+				if (isa<LandingPadInst>(firstInstr)) {
+					firstInstr = firstInstr->getNextNode()->getNextNode();
+				}
+				generalBuilder.SetInsertPoint(firstInstr);
+				generalBuilder.CreateCall(M.getFunction("pthread_mutex_lock"), ArrayRef<Value*>(mutex));
+
+				for (auto pred : predecessors(basicBlock)) {
+					auto syncBlock = BasicBlock::Create(ctx, "sync", function);
+					generalBuilder.SetInsertPoint(syncBlock);
+					generalBuilder.CreateCall(M.getFunction("pthread_mutex_unlock"), ArrayRef<Value*>(mutex));
+					block2sync[pred].push_back(syncBlock);
+					// return?
+				}
+			}
+		}
+
+		// DEBUG
+		// outs() << "\n";
+		// function->print(outs());
+		// outs() << "\n";
 
 		//
 		// move basic blocks to new function and create call from func
 		//
 		for (auto basicBlock : basicBlocks) {
 
-			// handle return type
-			auto retType = Type::getVoidTy(ctx);
-			auto lastInstr = &*--basicBlock->end();
-			if (isa<ReturnInst>(lastInstr)) {
-				auto retInstr = cast<ReturnInst>(lastInstr);
-				if (retInstr->getReturnValue()) {
-					retType = retInstr->getReturnValue()->getType();
-				}
-			}
-
 			// create function and move basic block to it
-			auto funcType = FunctionType::get(retType, false);
+			auto funcType = FunctionType::get(Type::getVoidTy(ctx), false);
 			auto outFunc = Function::Create(funcType, GlobalValue::LinkageTypes::PrivateLinkage, "newFunc", M);
-			auto syncBlock = BasicBlock::Create(ctx, "sync", outFunc);
-			basicBlock->moveAfter(syncBlock);
+			auto tmpBlock = BasicBlock::Create(ctx, "", outFunc);
+			basicBlock->moveAfter(tmpBlock);
+			tmpBlock->eraseFromParent();
 
-			// TODO: create sync block
-			IRBuilder<> syncBuilder(syncBlock);
-			syncBuilder.CreateBr(basicBlock);
+			// create sync block
+			// IRBuilder<> syncBuilder(syncBlock);
+			// syncBuilder.CreateBr(basicBlock);
 
-			// handle resume terminator (TODO: test error)
 			auto instr = &*std::prev(basicBlock->end(), 1);
-			if (isa<ResumeInst>(instr)) {
+			if (isa<ResumeInst>(instr)) {  // handle resume terminator (TODO: test error)
 				outFunc->setPersonalityFn(function->getPersonalityFn());
+			} else if (isa<BranchInst>(instr)) {  // handle branch
+				auto branch = cast<BranchInst>(instr);
+				for (unsigned i = 0; i < branch->getNumSuccessors(); i++) {
+					block2sync[basicBlock].at(i)->moveAfter(basicBlock);
+					branch->setSuccessor(0, block2sync[basicBlock].at(i));
+				}
 			}
 
 			// handle invoke terminator
@@ -274,19 +330,10 @@ PreservedAnalyses FlowObfuscatorPass::run(Module &M, ModuleAnalysisManager &AM) 
 			args.push_back(ConstantPointerNull::get(Type::getInt8PtrTy(ctx)));
 			auto pCreate = M.getFunction("pthread_create");
 			auto call = mainBuilder.CreateCall(pCreate, args);
-
-			// check if function returns and handle return
-			// if (!outFunc->getReturnType()->isVoidTy()) {
-			// 	mainBuilder.CreateRet();
-			// }
 		}
 
-		// DEBUG
-		auto retType = function->getReturnType();
-		if (retType->isVoidTy()) {
-			mainBuilder.CreateRetVoid();
-		} else {
-			mainBuilder.CreateRet(ConstantAggregateZero::get(retType));
+		if (retVal) {
+			mainBuilder.CreateRet(mainBuilder.CreateLoad(retVal));
 		}
 
 		function->print(outs());  // DEBUG
