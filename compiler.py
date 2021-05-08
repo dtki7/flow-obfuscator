b4da63809b0cfa94a2e911cc4e1bf84927014d43
oli <oli@server>
2021-05-08 19:26:43 +0200

    llvm_includes

--- compiler.py
+++ compiler.py
@@ -3,16 +3,19 @@
 import subprocess
 import sys
 
+
 FLAGS = "-pthread -O2"
+LLVM_INCLUDES = " -I/home/user/devel/llvm/llvm-project-10.0.0/build/lib/clang/10.0.0/include/"
+
 targets = {
-    "clang": "clang-10",
-    "clang32": "clang-10 -m32",
+    "clang": "clang-10" + LLVM_INCLUDES,
+    "clang32": "clang-10 -m32" + LLVM_INCLUDES,
     "gcc": "gcc",
     "gcc32": "gcc -m32",
-    "opt": "clang-opt",
-    "opt32": "clang-opt -m32",
-    "opt-main": "clang-opt-main",
-    "opt-main32": "clang-opt-main -m32"
+    "opt": "clang-opt" + LLVM_INCLUDES,
+    "opt32": "clang-opt -m32" + LLVM_INCLUDES,
+    "opt-main": "clang-opt-main" + LLVM_INCLUDES,
+    "opt-main32": "clang-opt-main -m32" + LLVM_INCLUDES
 }
 
 PRE = ""
