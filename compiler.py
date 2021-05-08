fa6f32c76dc8554d6ac1b862e0e3538539d30483
oli <oli@server>
2021-05-08 20:20:23 +0200

    compiler cpp support

--- compiler.py
+++ compiler.py
@@ -18,10 +18,23 @@ targets = {
     "opt-main32": "clang-opt-main -m32" + LLVM_INCLUDES
 }
 
+targets_pp = {
+    "clang": "clang++-10" + LLVM_INCLUDES,
+    "clang32": "clang++-10 -m32" + LLVM_INCLUDES,
+    "gcc": "g++",
+    "gcc32": "g++ -m32",
+    "opt": "clang-opt++" + LLVM_INCLUDES,
+    "opt32": "clang-opt++ -m32" + LLVM_INCLUDES,
+    "opt-main": "clang-opt-main++" + LLVM_INCLUDES,
+    "opt-main32": "clang-opt-main++ -m32" + LLVM_INCLUDES
+}
+
 PRE = ""
 
 def main():
     source_files = " ".join(sys.argv[1:])
+
+    targets = targets
 
     for target in targets.keys():
         cmd = "%s %s -o %s-%s %s" % (targets[target], FLAGS, PRE, target, source_files)
